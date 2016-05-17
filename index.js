80d6cb026dd52a7444f7d146dd0fa61199926c05
Oggi Danailov <ogidan@abv.bg>
2016-05-16 23:45:01 -0400

    all play buttons fixed!

--- index.js
+++ index.js
@@ -15,9 +15,55 @@ $( document ).ready(function(){
 });
 
 //play buttons here
+var audio1 = document.getElementById('audio1');
+var front = document.getElementById('frontAudio');
+var back = document.getElementById('back');
+
+var frontButton1 = document.getElementById('playButton1');
+var backButton1 = document.getElementById('pauseButton1');
+var rewind1 = document.getElementById('rewindButton1')
 
 
 
+frontButton1.addEventListener('click', toggleFront);
+backButton1.addEventListener('click', toggleBack);
+rewind1.addEventListener('click', startOver);
+
+
+// this is the play audio
+function playButton(){
+	audio1.play();
+	
+	};
+
+// this is the pause audio
+function pauseButton() {
+	audio1.pause();
+	
+}
+
+// this fuctions flips the image when I press play
+function toggleFront(){
+	if(front.style.display != 'none'){
+		back.style.display = 'block';
+		front.style.display = 'none'
+	}
+};
+
+// this function flips the image back when I press the pause button
+function toggleBack(){
+	if(back.style.display != 'none'){
+		front.style.display = 'block';
+		back.style.display = 'none'
+	}
+};
+
+
+// this is the rewind button; stops the music and rewinds from the beginning
+    function startOver(){
+    	audio1.currentTime = 0;
+    	audio1.pause();
+    }
 
 
 //end of play buttons here
--- index2.js
+++ index2.js
@@ -15,62 +15,99 @@
 // }
 
 
+
 // This are the audio buttons
 
-var playBut = document.querySelectorAll('#play');
-var audio = document.getElementById('audio');
-var a = document.getElementById('frontAudio');
-var b = document.getElementById('back');
-var frontButton = document.getElementById('playButton');
-var backButton = document.getElementById('pauseButton');
-var rewind = document.getElementById('rewindButton')
-var elementIsClicked = false;
-console.log('audio/Pineapple Rag.wav', audio.src)
 
-frontButton.addEventListener('click', toggleFront);
-backButton.addEventListener('click', toggleBack)
-rewind.addEventListener('click', startOver)
+var audio1 = document.getElementById('audio1');
+var front1 = document.getElementById('frontAudio1');
+var back1 = document.getElementById('back1');
+var frontButton1 = document.getElementById('playButton1');
+var backButton1 = document.getElementById('pauseButton1');
+var rewind1 = document.getElementById('rewindButton1')
 
-// Here I am looping through all the play buttons; querySelectorAll gives you an array
-for(i=0;i<playBut.length;i++){
-playBut[i].addEventListener('click', clickCard);
+var audio2 = document.getElementById('audio2');
+var front2 = document.getElementById('frontAudio2');
+var back2 = document.getElementById('back2');
+var frontButton2 = document.getElementById('playButton2');
+var backButton2 = document.getElementById('pauseButton2');
+var rewind2 = document.getElementById('rewindButton2')
+
+var audio3 = document.getElementById('audio3');
+var front3 = document.getElementById('frontAudio3');
+var back3 = document.getElementById('back3');
+var frontButton3 = document.getElementById('playButton3');
+var backButton3 = document.getElementById('pauseButton3');
+var rewind3 = document.getElementById('rewindButton3')
+
+function audioMelange(audio, front, back, frontButton, backButton, rewindButton){
+	frontButton.addEventListener('click', toggleFront);
+	backButton.addEventListener('click', toggleBack);
+	rewindButton.addEventListener('click', startOver);
+	
+	
+	function toggleFront(){
+		audio.play();
+		if(front.style.display != 'none'){
+		back.style.display = 'block';
+		front.style.display = 'none'
+		}
+	}
+	function toggleBack(){
+		audio.pause();
+	if(back.style.display != 'none'){
+		front.style.display = 'block';
+		back.style.display = 'none'
+		}
+	}
+	function startOver(){
+    	audio1.currentTime = 0;
+    	audio1.pause();
+    }
 }
 
-function clickCard(){
-	elementIsClicked = true;
-}
+audioMelange(audio1,front1, back1, frontButton1, backButton1, rewindButton1 );
+audioMelange(audio2,front2, back2, frontButton2, backButton2, rewindButton2 );
+audioMelange(audio3,front3, back3, frontButton3, backButton3, rewindButton3 );
+
+
+
+// frontButton1.addEventListener('click', toggleFront);
+// backButton1.addEventListener('click', toggleBack);
+// rewind1.addEventListener('click', startOver);
+
 
 // this is the play audio
-function playButton(){
-	audio.play();
-	};
+// function playButton(){
+// 	audio1.play();
+// 	};
 
 // this is the pause audio
-function pauseButton() {
-	audio.pause()
-}
+// function pauseButton() {
+// 	audio1.pause();	
+// }
 
 // this fuctions flips the image when I press play
-function toggleFront(){
-	if(a.style.display != 'none'){
-		b.style.display = 'block';
-		a.style.display = 'none'
-	}
-};
+// function toggleFront(){
+// 	if(front.style.display != 'none'){
+// 		back.style.display = 'block';
+// 		front.style.display = 'none'
+// 	}
+// };
 
 // this function flips the image back when I press the pause button
-function toggleBack(){
-	if(b.style.display != 'none'){
-		a.style.display = 'block';
-		b.style.display = 'none'
-	}
-};
+// function toggleBack(){
+// 	if(back.style.display != 'none'){
+// 		front.style.display = 'block';
+// 		back.style.display = 'none'
+// 	}
+// };
 
 
 // this is the rewind button; stops the music and rewinds from the beginning
-    function startOver(){
-    	audio.currentTime = 0;
-    	audio.pause();
-    }
+    // function startOver(){
+    // 	audio1.currentTime = 0;
+    // 	audio1.pause();
+    // }
 
 // audio buttons part comes to an end here
